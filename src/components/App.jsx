856110d521756fc47dac4a76fb63f2a2ca1f282c
SungJung <sungung0616@gmail.com>
2024-03-22 13:23:53 +0900

    Refactored the application to dynamically generate Note components using the map method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
+console.log(notes);
+
+function createNote (noteItem){
+    return   <Note
+        key = {noteItem.id}
+       title = {noteItem.title}
+       content = {noteItem.content}
+    />
+}
 
 function App() {
     return <div>
         <Header />
-        <Note />
+        {notes.map(createNote)}
         <Footer />
     </div>
   
